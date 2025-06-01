ee910fa84f5e679db529321cd0744e08cec83859
Neil Mitash <nilay_mitash@hotmail.com>
2025-06-01 10:03:00 -0400

    Basic array operations

---
+++ set/Set.java
@@ -0,0 +1,24 @@
+package set;
+
+/**
+ * This is an Array-based String Set.
+ * Rules: 
+ * 1. Cannot have a duplicate value.
+ */
+public class Set {
+
+    private String[] set;
+
+    constructor(int size) {
+        set = String[size];
+    }
+
+    //TODO: check the default constructor - if it works, I dont think it would, but if it does, throw an exception.
+
+    //add
+
+    //remove
+
+    //update??
+
+}
